03e325d9c2d13102879b26f07424c8bebf9f3fe6
DevAbdulAlimRony <alim15@cse.pstu.ac.bd>
2024-10-08 17:23:24 +0600

    make username from name using spatie sluggable

--- app/Http/Requests/ProfileUpdateRequest.php
+++ app/Http/Requests/ProfileUpdateRequest.php
@@ -17,6 +17,7 @@ class ProfileUpdateRequest extends FormRequest
     {
         return [
             'name' => ['required', 'string', 'max:255'],
+            'username' => ['required', 'string', 'max:255', 'regex:/^[a-z0-9\-\.]+$/i'],
             'email' => [
                 'required',
                 'string',
@@ -25,6 +26,12 @@ class ProfileUpdateRequest extends FormRequest
                 'max:255',
                 Rule::unique(User::class)->ignore($this->user()->id),
             ],
+        ];
+    }
+
+    public function messages(){
+        return [
+            'regex' => "username can only contain alphanumecir characters, dash and dot"
         ];
     }
 }
